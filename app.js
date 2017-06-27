14f08f25f52e72420d75eb48311b202a2e43f00f
Kchalmers <kevinqchal@yahoo.com>
2017-06-26 22:02:08 -0700

    weather is functional for C and F

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 function Weather() {
+    const self = this;
+    let currentTemp = 0;
+
+    this.initialize = function () {
+        $(".symbolLink").click(this.changeFormat);
+    };
     this.getLocation = function () {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(this.getWeatherData);
@@ -7,21 +13,44 @@ function Weather() {
         }
     };
     this.getWeatherData = function (position) {
+        // var api_key = "ead0bb7ea1756dceb0d6073a367b9272";
         $.ajax({
             dataType: "JSON",
-            method: "GET",
-            url: `https://api.openweathermap.org/data/2.5/weather?lat=${Math.floor(position.coords.latitude)}&lon=${Math.floor(position.coords.longitude)}&id=524901&APPID={ead0bb7ea1756dceb0d6073a367b9272}`,
+            method: "POST",
+            url: `http://api.openweathermap.org/data/2.5/weather?lat=${Math.floor(position.coords.latitude)}&lon=${Math.floor(position.coords.longitude)}&appid=${weatherApiKey}`,
             success: function (response) {
-                console.log("Random pet", response);
+                currentTemp =  response.main.temp;
+                self.tempFormat(response.main.temp);
+                self.displayWeatherImg(response.weather["0"].main);
             },
             error: function (response) {
                 console.log(response);
             }
         })
     };
+    this.tempFormat = function (temp, symbol) {
+        const tempHolder = $("temp");
+        let convertedTemp = 0;
+        if(symbol === "F"){
+            convertedTemp = Math.floor(temp - 273.15);
+            symbol = "C";
+        } else {
+            convertedTemp = Math.floor(temp * (9/5) - 459.67);
+            symbol = "F";
+        }
+        tempHolder.append($(".tempNum").empty().append(`${convertedTemp} ${String.fromCharCode(176)}`));
+        tempHolder.append($(".symbolLink").empty().append(symbol));
+    };
+    this.changeFormat = function () {
+        self.tempFormat(currentTemp, document.getElementsByClassName("symbolLink")[0].innerHTML);
+    };
+    this.displayWeatherImg = function (weatherImg) {
+        console.log(weatherImg)
+    };
 }
 
 $(document).ready(function () {
    const start = new Weather();
+    start.initialize();
    start.getLocation();
 });
